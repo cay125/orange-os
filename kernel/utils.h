8556d942ee8813f8fe76e7f1c81bf6f0333d1d76
xiangpu <xiangpu_zh@foxmail.com>
2023-01-29 20:28:43 +0800

    add interrupt func

--- kernel/utils.h
+++ kernel/utils.h
@@ -1,9 +1,19 @@
 #ifndef KERNEL_UTILS_H
 #define KERNEL_UTILS_H
 
+#include "arch/riscv_reg.h"
+
 namespace kernel {
 
 void panic();
+
+inline __attribute__((always_inline)) void global_interrunpt_on() {
+  riscv::regs::mstatus.set_bit(riscv::StatusBit::mie);
+}
+
+inline __attribute__((always_inline)) void global_interrunpt_off() {
+  riscv::regs::mstatus.clear_bit(riscv::StatusBit::mie);
+}
 
 }  // namespace kernel
 
