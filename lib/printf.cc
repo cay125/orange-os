d239ea1a1280fdffc1a386c3dcb03c5f449f2bbb
xiangpu <xiangpu_zh@foxmail.com>
2023-01-29 20:52:49 +0800

    optim printf impl

--- lib/printf.cc
+++ lib/printf.cc
@@ -1,13 +1,14 @@
 #include "lib/lib.h"
 
 #include "lib/printk.h"
+#include "lib/string.h"
 
 void printf(const char *fmt, ...) {
 	va_list va;
 	va_start(va, fmt);
-	auto f = [](char c){
-	  syscall::write(1, &c, 1);
-	};
-	simple_vsprintf(nullptr, fmt, va, f);
+	char out_str[64] = {0};
+	char* p = out_str;
+	simple_vsprintf(&p, fmt, va, nullptr);
 	va_end(va);
+	syscall::write(1, out_str, strlen(out_str));
 }
