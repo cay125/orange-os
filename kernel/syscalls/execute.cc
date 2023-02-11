0679927f5e26023e3a20806c9fe53c5e1d081287
xiangpu <xiangpu_zh@foxmail.com>
2023-02-11 22:42:29 +0800

    rename exception_table

--- kernel/syscalls/execute.cc
+++ kernel/syscalls/execute.cc
@@ -17,7 +17,7 @@
 
 extern "C" {
 void restore_user_context(kernel::RegFrame* frame);
-void exception_table();
+void user_exception_table();
 }
 
 namespace kernel {
@@ -116,7 +116,7 @@ void TrapRet(ProcessTask* process, riscv::Exception exception) {
     riscv::isa::sfence();
   }
   riscv::regs::mepc.write(frame->mepc);
-  riscv::regs::mtvec.write(reinterpret_cast<uint64_t>(exception_table) + 1);
+  riscv::regs::mtvec.write(reinterpret_cast<uint64_t>(user_exception_table) + 1);
   riscv::regs::mstatus.set_mpp(riscv::MPP::user_mode);
   riscv::regs::mstatus.clear_bit(riscv::StatusBit::mpie);
   restore_user_context(frame);
